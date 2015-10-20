2b9f8f826eef6521247aed0016143fa7ac9ffb91
Omrika <omrikafolkes@gmail.com>
2015-10-19 22:32:02 -0400

    click button and open tab url's are listed in popup

--- popup.js
+++ popup.js
@@ -1,16 +1,27 @@
-document.getElementById("create").addEventListener("click", function(){
-    var $arr = [];
+//the open tabs url is now printing to the console
+document.getElementById('create').addEventListener('click', function(){
+    var arr = [];
+
     chrome.tabs.query({}, function(tabs){
         for (var i = 0; i < tabs.length; i++){
-            $arr[i] = tabs[i];
+            arr[i] = tabs[i];
         }
         
-        for (var i = 0; i < $arr.length; i++){
-            if ($arr[i] != null) {
-                console.log($arr[i].url);
+        for (var i = 0; i < arr.length; i++){
+            if (arr[i] != null) {
+                var div = document.createElement('div')
+                div.className = 'list'
+                div.innerHTML = "<ul><li>" + arr[i].url +"</li></ul>";
+                document.getElementById('content').appendChild(div);
             } else {
-                console.log("??" + i);
+                document.write("no tabby's");
             }
         }
     })
-});
+
+}); 
+ 
+//whats next..
+//create a dropdown 
+//different drop down tabs
+//should be able to add urls to specified tabs...
