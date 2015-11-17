911ce951e1d01e4c9755903c9ac29df43160ea6f
Omrika <omrikafolkes@gmail.com>
2015-11-17 09:34:58 -0500

    toggle show/hide field

--- popup.js
+++ popup.js
@@ -1,28 +1,30 @@
 document.getElementById('create').addEventListener('click', function(){
-    var arr = [];
+    // var arr = [];
 
-    chrome.tabs.query({}, function(tabs){
-        for (var i = 0; i < tabs.length; i++){
-            arr[i] = tabs[i];
-        }
+    // chrome.tabs.query({}, function(tabs){
+    //     for (var i = 0; i < tabs.length; i++){
+    //         arr[i] = tabs[i];
+    //     }
         
-        for (var i = 0; i < arr.length; i++){
-            if (arr[i] != null) {
-                var div = document.createElement('div')
-                div.className = 'list'
-                div.innerHTML = "<ul><li>" + arr[i].url +"</li></ul>";
-                document.getElementById('content').appendChild(div);
-            } else {
-                document.write("no tabby's");
-            }
+    //     for (var i = 0; i < arr.length; i++){
+    //         if (arr[i] != null) {
+    //             var div = document.createElement('div')
+    //             div.className = 'list'
+    //             div.innerHTML = "<ul><li>" + arr[i].url +"</li></ul>";
+    //             document.getElementById('content').appendChild(div);
+    //         } else {
+    //             document.write("no tabby's");
+    //         }
+    //     }
+    // })
+    var field = document.getElementById('title');
+        if(field.style.display == 'block') {
+          field.style.display = 'none';
         }
-    })
+        else {
+            field.style.display = 'block'
+        }
 
-}); 
- 
-
-document.getElementById('list').addEventListener('click', function(){
-    
 }); 
 
 
