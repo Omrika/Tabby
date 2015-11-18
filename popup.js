545c8a922a6a8b87b85de2328ca495ec95de2a06
Omrika <omrikafolkes@gmail.com>
2015-11-17 20:56:03 -0500

    submit btn creates group

--- popup.js
+++ popup.js
@@ -25,14 +25,10 @@ document.getElementById('create').addEventListener('click', function() {
 
     var arr = [];
 
-
-
     chrome.tabs.query({'active': false}, function(tabs) { //chrome.tabs.query is a chrome api method. query() gets the tabs.id. query() is asynchronous      
         for (var i = 0; i < tabs.length; i++) {
             arr.push(tabs[i]);
         }
-        // debugger
-
 
         for (var i = 0; i < arr.length; i++) {
             if (arr[i] != null) {
@@ -40,12 +36,48 @@ document.getElementById('create').addEventListener('click', function() {
                 div.className = 'list'
                 div.innerHTML = "<input type='checkbox' id='box'>" + arr[i].url + ">"; //grabs each tab url.
                 document.getElementById('content').appendChild(div);
-
             }
         }
-
     })
 });
+
+//when user clicks the submit button. The data in the input field and the checked off boxes
+//should be saved with localStorage
+//retrieved with localStorage and put in a drop down.
+
+document.getElementById('submit').addEventListener('click', function(){
+    var check_bool = document.getElementById('box')
+    var field_data = document.getElementById('title')
+    var wrapper = document.getElementById('wrapper')
+    var panel = document.getElementById('accordion')
+    var panel_heading = document.getElementById('collapsible')
+    var panel_body = document.getElementById('panel_body')
+
+    //when i click submit i want to get the value of the field and check if box is checked
+    //and save both in a dropdown list. 
+
+    
+    if ( check_bool.checked) { // or if field has text in it.
+        panel_heading.innerHTML = field_data.value //ield data value is now the name of the group
+        wrapper.style.display = 'none'; //hide 'create group' info
+        panel.style.display = 'block'; //show the list of groups
+    }
+    
+    //test^^
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function clearAllLists() {
 //     var lists = document.getElementById('content').getElementsByClassName('list');
