8dd1543e1134090128a80ca02169690317eb304e
Omrika <omrikafolkes@gmail.com>
2015-11-23 20:48:55 -0500

    localstorage stores users input

--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
+field_data = document.getElementById( 'title' )
+wrapper = document.getElementById( 'wrapper' )
+panel = document.getElementById( 'accordion' )
+panel_heading = document.getElementById( 'collapsible' )
+panel_body = document.getElementById( 'accordionOne' )
 var clicked = false;
+
 
 document.getElementById( 'create' ).addEventListener( 'click', function() {
 //click event on the create button.
@@ -10,7 +16,6 @@ document.getElementById( 'create' ).addEventListener( 'click', function() {
     //click is originally false because i didn't click. But after i add event listener and i click
     //it becomes true. Then i can no longer click. This prevents urls to be repeatedly added to the list
     //when the button is clicked. So i disabled it.
-
 
     var field = document.getElementById( 'title' );
     var button = document.getElementById( 'submit' );
@@ -45,37 +50,76 @@ document.getElementById( 'create' ).addEventListener( 'click', function() {
 //should be saved with localStorage
 //retrieved with localStorage and put in a drop down.
 
+// field_data = document.getElementById( 'title' )
+// wrapper = document.getElementById( 'wrapper' )
+// panel = document.getElementById( 'accordion' )
+// panel_heading = document.getElementById( 'collapsible' )
+// panel_body = document.getElementById( 'accordionOne' )
+
 document.getElementById( 'submit' ).addEventListener( 'click', function(){
+    
     var check_bool = document.getElementById( 'box' )
-        field_data = document.getElementById( 'title' )
-        wrapper = document.getElementById( 'wrapper' )
-        panel = document.getElementById( 'accordion' )
-        panel_heading = document.getElementById( 'collapsible' )
-        panel_body = document.getElementById( 'accordionOne' )
-        // urls = document.querySelectorAll('#box')
 
     //when i click submit i want to get the value of the field and check if box is checked
     //and save both in a dropdown list. 
-
-    
+  
     if ( check_bool.checked || field_data != undefined ) { // or if field has text in it.
-        panel_heading.innerHTML = field_data.value; //field data value is now the name of the group
-        // panel_body.innerHTML = "<ul><li>" + check_bool.name + "</li></ul>";
+
+        //field data value is now the name of the group
         var selected = []
         
         var u = $(".list").children("input:checked").each(function(){ selected.push(($(this).attr('name')))})
-        // for(var i = 0; i < u.length; i++){(panel_body.innerHTML = "<ul><li>" + (u[i].name) + "</li></ul>")}
-        panel_body.innerHTML = selected;
+
+        localStorage['field'] = field_data.value;
+        localStorage['selected'] = selected;
+        
+        panel_heading.innerHTML = localStorage['field'];
+        panel_body.innerHTML = localStorage['selected'];
+        //^ sets input in heading and body. 
+        // chrome.storage.local.set({'field', field_data.value} function(){})
+
+
 
         wrapper.style.display = 'none'; //hide 'create group' info
-        panel.style.display = 'block'; //show the list of groups
+        panel.style.display = 'block'; //show the list of groups  
     }
-        debugger
-    
-    //test^^
 
 });
 
+
+
+// window.onload = function(){
+//     if (field_data.value != undefined || selected != undefined){
+//         //if both aren't undefined then i want to get the storage of data
+//         var field = field_data.value
+
+//       }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//problem: im setting and geeting the field data but it overwrites it everytime i create new field data
+//and its not rendering to the page.
 
 //to do:
 //only shows first checkbox clicked if i click more than one checkbox. so figure out a way
@@ -84,14 +128,11 @@ document.getElementById( 'submit' ).addEventListener( 'click', function(){
 //make url list in groups links so user can click on them
 //
 
+// urls = document.querySelectorAll('#box')
 
 
 
-
-
-
-
-
+//if panel = document.getElementById('accordion') has a group then show it. 11/2-
 
 // function clearAllLists() {
 //     var lists = document.getElementById('content').getElementsByClassName('list');
